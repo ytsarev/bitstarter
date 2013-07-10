081e4798ed425d610d9ec9c0fb12201d70ac17a6
Yury Tsarev <ytsarev@suse.com>
2013-07-11 00:37:30 +0200

    Output index.html
    Signed-off-by: Yury Tsarev <ytsarev@suse.com>

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  index = fs.readFileSync('./index.html');
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
